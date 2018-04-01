f8eb845af45a94eed2207f12888712d28bcd75c2
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-04-01 19:41:36 +0300

    changed access qualifier

--- core/src/main/scala/org/meerkat/util/wrappers/SPPFToTreesBFSIterator.scala
+++ core/src/main/scala/org/meerkat/util/wrappers/SPPFToTreesBFSIterator.scala
@@ -65,12 +65,15 @@ private class Context(startQueue: mutable.Queue[SubtreeNode], startRoot: Subtree
   }
 }
 
-private class SPPFToTreesBFSIterator(root: NonPackedNode) extends Iterator[Tree] {
+class SPPFToTreesBFSIterator(root: NonPackedNode) extends Iterator[Tree] {
   override def hasNext: Boolean = !contextQueue.isEmpty
 
-  override def next(): Tree = Stream.iterate(Option.empty[SubtreeNode])(_ => step()).flatten.head.toTrees.head
+  override def next(): Tree =
+    Stream.iterate(Option.empty[SubtreeNode])(_ => step()).
+      flatten.head.toTrees.head
 
-  private val contextQueue: mutable.Queue[Context] = mutable.Queue(new Context(new SubtreeNode(root)))
+  private val contextQueue: mutable.Queue[Context] =
+    mutable.Queue(new Context(new SubtreeNode(root)))
 
   private def step(): Option[SubtreeNode] = {
     val currentContext = contextQueue.dequeue()
